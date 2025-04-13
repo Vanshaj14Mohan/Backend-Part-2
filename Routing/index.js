cfc100e35757cc2de3edcdbbefab0a826a420a81
Vanshaj P Mohan <VanshajKumar145@gmail.com>
2025-04-14 00:18:20 +0530

    Routing

--- Routing/index.js
+++ Routing/index.js
@@ -2,8 +2,31 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+//Creating different routes now:
 app.get('/', (req, res) => {
-  res.send('Route Path')
+  res.send('Route Path');
+})
+
+app.get("/about", (req,res)=>{
+    res.send("You contacted about page");
+})
+
+app.get("/contact", (req,res)=>{
+    res.send("You contacted contact page");
+})
+
+app.get("/search", (req,res)=>{
+    res.send("You searched for something");
+})
+
+//"*" means to include all the routes incluing above ones and if someone sends req on any other route, it will show this message.
+// app.get("*", (req,res)=>{
+//     res.send("404 Error Path does not exist")
+// })
+
+//Also a post request can be created in the same way as get request
+app.post("/", (req,res)=>{
+    res.send("You send a post request to the root");
 })
 
 app.listen(port, () => {
